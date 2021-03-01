8aeca2ccca2baf35f82baf7d881a080f376cb97a
Maruf Ozkan <74539562+cevdetoz@users.noreply.github.com>
2021-03-01 12:31:22 +0200

    Fort commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -9,7 +9,8 @@ public class FirstClass {
         System.out.println("third commit from local master");
         System.out.println("Frist commit from remote master branch");
         System.out.println("second commit from remote master branch");
-		System.out.println("Thirt commit from remote master branch");
+      System.out.println("Thirt commit from remote master branch");
+		System.out.println("Here is the conflict we have from remote master branch");
 		
 	}
 
