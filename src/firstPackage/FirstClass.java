1563fc2d827a7a9dca657d3fc237182f15e43b2a
cevdetoz <cevdetoz047@gmail.com>
2021-03-01 11:31:22 +0200

    third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first comment master branch");
         System.out.println("second commit from local master");
-        
+        System.out.println("third commit from local master");
 	}
 
 }
