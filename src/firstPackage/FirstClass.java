fd538f63b7b5205db3fcde6d74565e13385fbcd1
Maruf Ozkan <74539562+cevdetoz@users.noreply.github.com>
2021-03-01 12:57:56 +0200

    fifth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,17 +4,17 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("first comment master branch");
-        System.out.println("second commit from local master");
-        System.out.println("third commit from local master");
-        System.out.println("Frist commit from remote master branch");
-        System.out.println("second commit from remote master branch");
-		System.out.println("Thirt commit from remote master branch");
-		System.out.println("Here is a merge confict i create from local master");
+ System.out.println("first comment master branch"); System.out.println("second commit from local master");
+System.out.println("third commit from local master");
+System.out.println("Frist commit from remote master branch");
+System.out.println("second commit from remote master branch");
+System.out.println("Thirt commit from remote master branch");
+System.out.println("Here is a merge confict i create from local master");
 		
 
-      System.out.println("Thirt commit from remote master branch");
-		System.out.println("Here is the conflict we have from remote master branch");
+ System.out.println("Thirt commit from remote master branch");
+System.out.println("Here is the conflict we have from remote master branch");
+System.out.println("Here is anathor confict from remote master branch");
 
 		
 	}
