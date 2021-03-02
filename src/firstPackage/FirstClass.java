de853d2a98c8aeafc6d9b8b912a5ed3faeb1338e
cevdetoz <cevdetoz047@gmail.com>
2021-03-02 07:42:55 +0200

    firt commit from local feature branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -5,21 +5,21 @@ public class FirstClass {
 
 	public static void main(String[] args) {
  System.out.println("first comment master branch"); System.out.println("second commit from local master");
-System.out.println("third commit from local master");
-System.out.println("Frist commit from remote master branch");
-System.out.println("second commit from remote master branch");
-System.out.println("Thirt commit from remote master branch");
-System.out.println("Here is a merge confict i create from local master");
+ System.out.println("third commit from local master");
+ System.out.println("Frist commit from remote master branch");
+ System.out.println("second commit from remote master branch");
+ System.out.println("Thirt commit from remote master branch");
+ System.out.println("Here is a merge confict i create from local master");
 		
 
 
-      System.out.println("Thirt commit from remote master branch");
-		System.out.println("Here is the conflict we have from remote master branch");
-System.out.println("here is another confilct from local master branch");
+   System.out.println("Thirt commit from remote master branch");
+   System.out.println("Here is the conflict we have from remote master branch");
+   System.out.println("here is another confilct from local master branch");
 
- System.out.println("Thirt commit from remote master branch");
-System.out.println("Here is the conflict we have from remote master branch");
-
+   System.out.println("Thirt commit from remote master branch");
+   System.out.println("Here is the conflict we have from remote master branch");
+   System.out.println("here are changes from local feature branch");
 
 		
 	}
