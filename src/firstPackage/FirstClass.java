41d91476c142aa236adb6624a739078c0ef09871
Maruf Ozkan <74539562+cevdetoz@users.noreply.github.com>
2021-03-02 10:21:57 +0200

    secondt conflit
    here is the uptade about conflict on remote master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -25,9 +25,10 @@ public class FirstClass {
 
    System.out.println("here are changes from local feature branch"); 
    System.out.println("here are same changes from remote master branch");
+  System.out.println("here is my  second confilit remote feature branch");
     
 
-		
+	System.out.println("here is my  second confilit remote feature branch");	
 	}
 
 }
